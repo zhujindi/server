eeba5b2b158fe2da72854e788adcc3c462fc3747
Sergey Vojtovich <svoj@mariadb.org>
2019-12-21 01:22:09 +0400

    Fixed close_cached_connection_tables() flushing
    Let DROP SERVER and ALTER SERVER perform fair affected tables flushing.
    That is acquire MDL_EXCLUSIVE and do tdc_remove_table(TDC_RT_REMOVE_ALL).
    
    Aim of this patch is elimination of another inconsistent use of
    TDC_RT_REMOVE_UNUSED. It fixes (to some extent) a problem described in the
    beginning of sql_server.cc, when close_cached_connection_tables()
    interferes with concurrent transaction.
    
    A better fix should probably introduce proper MDL locks for server
    objects?
    
    Part of MDEV-17882 - Cleanup refresh version

--- sql/sql_base.cc
+++ sql/sql_base.cc
@@ -689,33 +689,21 @@ end:
   Close cached connections
 
   @return false  ok
-  @return true   If there was an error from closed_cached_connection_tables or
-                 if there was any open connections that we had to force closed
+  @return true   error, some tables may keep using old server info
 */
 
 bool close_cached_connection_tables(THD *thd, LEX_CSTRING *connection)
 {
-  bool res= false;
-  close_cached_connection_tables_arg argument;
+  close_cached_connection_tables_arg argument= { thd, connection, 0 };
   DBUG_ENTER("close_cached_connections");
-  DBUG_ASSERT(thd);
-
-  argument.thd= thd;
-  argument.connection= connection;
-  argument.tables= NULL;
 
   if (tdc_iterate(thd,
                   (my_hash_walk_action) close_cached_connection_tables_callback,
                   &argument))
     DBUG_RETURN(true);
 
-  for (TABLE_LIST *table= argument.tables; table; table= table->next_local)
-    res|= tdc_remove_table(thd, TDC_RT_REMOVE_UNUSED,
-                           table->db.str,
-                           table->table_name.str);
-
-  /* Return true if we found any open connections */
-  DBUG_RETURN(res);
+  DBUG_RETURN(close_cached_tables(thd, argument.tables, true,
+                                  thd->variables.lock_wait_timeout));
 }
 
 
